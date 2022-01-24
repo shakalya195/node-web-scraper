118805bc0076cc422819d5f81da210a778645587
shakalya <shakalya195@gmail.com>
2022-01-24 10:13:14 +0530

    scrapping 1mg

--- README.md
+++ README.md
@@ -9,4 +9,11 @@ $ npm install
 $ node server.js
 ```
 
- Then it will start up our node server, navigate to http://localhost:8081/scrape and see what happens.
+ Then it will start up our node server, navigate to http://localhost:8081/ and see what happens.
+
+
+ Put the URL like "https://www.1mg.com/sitemap.xml";
+ it will be collecting nested xml from the root sitemap.xml
+ and make an entry into mongodb database "node-web-scrapper" collection "urls" for each other links then xml.
+
+which cna be further used for crapping One by One.
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const https = require('https');
 const fs      = require('fs');
 const flow = require('xml-flow');
 const request = require('request');
@@ -7,15 +8,32 @@ const cheerio = require('cheerio');
 const transform = require('camaro');
 const app     = express();
 
+const mongoose = require('mongoose');
+
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost:27017/node-web-scrapper", {
+     useNewUrlParser: true,
+     useUnifiedTopology: true 
+     }).then(success => {
+    console.log('MONGO DB CONNECTED')
+}).catch(err => {
+    process.exit(1);
+});
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded());
 app.use(express.json());
 // use res.render to load up an ejs view file
 
+
+const UrlSchema = new mongoose.Schema({
+    url: {type: String, trim: true, default: null},
+});
+var UrlDocument = mongoose.model('url', UrlSchema);
+
 // index page
 app.get('/', function(req, res) {
-  var site = 'http://www.1mg.com/sitemap.xml';
+  var site = 'https://www.1mg.com/sitemap.xml';
   res.render('pages/index',{site:site});
 });
 
@@ -25,10 +43,12 @@ app.get('/about', function(req, res) {
 });
 
 var number=0;
+var links = [];
 app.post("/sitelist", async function(req, res) {
   // console.log(req.body);
   var url = req.body.url;
   var data = await scrapXMLUrl(url);
+  console.log('==============>',links.count);
   res.render('pages/single-xml', {
     data: data
   });
@@ -38,29 +58,39 @@ app.post("/sitelist", async function(req, res) {
 
 async function scrapXMLUrl(url){
   return new Promise((resolve, reject) => {
-      http.get(url, function(res) {
-        http.get(url, (xml) => {
-        var file = fs.createWriteStream('site.xml');
+        https.get(url, (res) => {
+        var filename = url.substring(url.lastIndexOf('/')+1);
+        var file = fs.createWriteStream(filename);
         res.on('data', function(data) {
             file.write(data);
         }).on('end', function() {
             file.end();
-            var inFile = fs.createReadStream("site.xml"),
+            var inFile = fs.createReadStream(filename),
                 xmlStream = flow(inFile);
             var array = [];
-            xmlStream.on('tag:loc', function(url) {
-                array.push({ id: number, url : url.$text });
-                number++;
+            xmlStream.on('tag:loc', async function(url) {
+              var ext = url.$text.split('.').pop().trim();
+              // console.log('================================>',ext)
+                if( ext == 'xml'){
+                  var arrayHTML = await scrapXMLUrl(url.$text);
+                  array = [...array, ...arrayHTML];
+                }else{
+                  array.push({ id: number, url : url.$text });
+                  number++;
+                  UrlDocument.collection.insertOne({url:url.$text},{});
+                }                
             });
             xmlStream.on('end', function(url) {
-                // console.log(array);
+                console.log(array);
+                links = [...links, ...array];
                 resolve(array);
             });  
         });
       });
-    });
+   
   });
 }
+
 
 app.get('/scrape', function(req, res){
   // Let's scrape Anchorman 2
